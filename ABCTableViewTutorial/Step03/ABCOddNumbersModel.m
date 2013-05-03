5671ae93fadb72d2f35533ba4db0050ca9b84da5
Neem Serra <neemserra@gmail.com>
2013-05-02 23:13:47 -0500

    Added tests and completed step 3

--- ABCTableViewTutorial/Step03/ABCOddNumbersModel.m
+++ ABCTableViewTutorial/Step03/ABCOddNumbersModel.m
@@ -11,7 +11,15 @@
 @implementation ABCOddNumbersModel
 
 - (NSArray *)oddIntegersFrom:(NSInteger)fromInteger to:(NSInteger)toInteger {
-    return @[@"one"];
+    NSInteger isEven = (fromInteger % 2 == 0);
+    if (isEven) {
+        ++fromInteger;
+    }
+    NSMutableArray *oddIntegerArray = [[NSMutableArray alloc] init];
+    for (int oddInteger = fromInteger; oddInteger < toInteger; oddInteger += 2) {
+        [oddIntegerArray addObject:[NSNumber numberWithInt:oddInteger]];
+    }
+    return oddIntegerArray;
 }
 
 @end
--- ABCTableViewTutorial/Step03/ABCOddNumbersModel03Tests.m
+++ ABCTableViewTutorial/Step03/ABCOddNumbersModel03Tests.m
@@ -23,4 +23,26 @@
     NSInteger countOfNumbers = [listOfNumbers count];
     GHAssertTrue(countOfNumbers > 0, @"We should have more than zero numbers.");
 }
+
+- (void)testOddIntegersFrom1To10Returns5Numbers {
+    NSArray *listOfOddNumbers = [self.testObject oddIntegersFrom:1 to:10];
+    
+    GHAssertEquals([listOfOddNumbers count], 5, @"We should end up with 5 odd integers.");
+}
+
+- (void)testOddIntegersFrom1To20Returns10Numbers {
+    NSArray *listOfNumbers = [self.testObject oddIntegersFrom:1 to:20];
+    
+    GHAssertEquals([listOfNumbers count], 10, @"We should have 10 numbers.");
+}
+
+- (void)testOddIntegersFrom14To22Returns4SpecificOddValues {
+    NSArray *expectedOddValues = @[@15,@17,@19,@21];
+    NSArray *actualOddValues = [self.testObject oddIntegersFrom:14 to:22];
+    NSInteger countOfNumbers = [actualOddValues count];
+    GHAssertEquals(expectedCountOfNumbers, 4, @"We should have 4 numbers.");
+    
+    GHAssertEqualObjects([actualOddValues count], expectedOddValues, @"Values should be equal");
+}
+
 @end
